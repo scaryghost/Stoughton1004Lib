3315ab1a4e8a6b931f674bc5b20368bba67f5c12
etsai <thegod0fde4th@gmail.com>
2012-04-25 22:26:22 -0700

    Skeleton for Graph class

---
+++ Graph/Graph.h
@@ -0,0 +1,17 @@
+#ifndef 1004STOUGHTONLIB_GRAPH_H
+#define 1004STOUGHTONLIB_GRAPH_H
+
+namespace 1004StoughtonLib {
+
+/**
+ * Represnts a collection of edges and vertices
+ * @author etsai
+ * @author sjiang
+ */
+class Graph {
+
+};  //class Graph
+
+}   //namespace 1004StoughtonLib
+
+#endif
