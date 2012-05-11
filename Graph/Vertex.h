72672fef125dcdd00a46a751798745f3f4ee205a
etsai <thegod0fde4th@gmail.com>
2012-05-10 23:41:07 -0700

    Added comments to the Vertex class

--- Graph/Vertex.h
+++ Graph/Vertex.h
@@ -9,16 +9,30 @@ namespace Stoughton1004Lib {
 
 class Edge;
 
+/**
+ * Represents a vertex in a graph
+ * @author etsai
+ * @author sjiang
+ */
 class Vertex : public UniqueId {
 public:
+    /** Default constructor */
     Vertex();
     
+    /**
+     * Add an edge to the vertex
+     * @param   edge    Edge to connect
+     */
     void addEdge(const Edge& edge);
+    /**
+     * Get the edges originating from the vertex
+     * @return  Vertex edges
+     */
     const std::vector<Edge>& getEdges() const;
 
     std::string virtual getName() const
 private:
-    std::vector<Edge> edges;
+    std::vector<Edge> edges;    ///< The edges originating from the vertex
 };  //class Vertex
 
 }   //namespace Stoughton1004Lib
