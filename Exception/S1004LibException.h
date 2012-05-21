f23533b5a6dba807805e0ec53fb62b3cf5d12830
etsai <thegod0fde4th@gmail.com>
2012-05-20 20:55:11 -0700

    Added doxygen comments to the class

--- Exception/S1004LibException.h
+++ Exception/S1004LibException.h
@@ -6,20 +6,32 @@
 
 namespace Stoughton1004Lib {
 
+/**
+ * Custom exception class for the Stoughton1004 library.
+ * @author etsai
+ */
 class S1004LibException : public std::exception {
 public:
+    /**
+     * Create exception with given error message
+     * @param   msg     Error message
+     */
     S1004LibException(const std::string& msg) {
         this->msg= msg;
     }
     ~S1004LibException() throw() {
     }
 
+    /**
+     * Get the error message
+     * @return  Error message for the exception
+     */
     const char* what() const throw() {
         return msg.c_str();
     }
 
 private:
-    std::string msg;
+    std::string msg;        ///< Stores the error message
 };  //class S1004LibException
 
 }   //namespace Stoughton1004Lib
