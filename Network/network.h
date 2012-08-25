e2be085a9b4f09ff71aa935bf609890f87699e36
Eric Tsai <thegod0fde4th@gmail.com>
2012-08-25 03:59:06 -0700

    Created cental header to include all other headers in the module

---
+++ Network/network.h
@@ -0,0 +1,8 @@
+#ifndef STOUGHTON1004LIB_NETWORK_H
+#define STOUGHTON1004LIB_NETWORK_H
+
+#include "Stoughton1004Lib/Network/DatagramPacket.h"
+#include "Stoughton1004Lib/Network/DatagramSocket.h"
+#include "Stoughton1004Lib/Network/InetAddress.h"
+
+#endif
