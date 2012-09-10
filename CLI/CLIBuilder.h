b9fdccb52ec700184d43cd0cba610e55be6edfde
etsai <thegod0fde4th@gmail.com>
2012-09-10 01:43:20 -0700

    Added doxygen comments

--- CLI/CLIBuilder.h
+++ CLI/CLIBuilder.h
@@ -9,24 +9,51 @@
 
 namespace Stoughton1004Lib {
 
+/**
+ * Builder for processing command line arguments
+ * @author etsai
+ */
 class CLIBuilder {
 public:
+    /**
+     * Get the builder object
+     * @return  CLIBuilder instance
+     */
     static CLIBuilder* getBuilder();
 
+    /**
+     * Add an option to the collection
+     * @param   opt     Option to add
+     * @return  Reference to the calling object
+     * @throw   S1004LibException   If a duplicate option has already been acted
+     */
     CLIBuilder& addOption(const Option& opt) throw(S1004LibException);
+    /**
+     * Set the usage message
+     * @param   usage   Usage description
+     * @return  Reference to the calling object
+     */
     CLIBuilder& setUsage(const std::string& usage);
 
+    /**
+     * Parse the command line arguments and execute the callback functions
+     * @param   argc    Number of arguments
+     * @para    argv    Array of arguments passed in
+     */
     void parse(int argc, char **argv);
+    /**
+     * Print the usage message, and then terminate
+     */
     void usage();
 
 private:
-    CLIBuilder();
+    CLIBuilder();                           ///< Default constructor
 
-    static CLIBuilder instance;
-    std::map<std::string, Option> options;
-    std::string usage;
-};
+    static CLIBuilder instance;             ///< Singleton instance of the class
+    std::map<std::string, Option> options;  ///< Map of added options
+    std::string usage;                      ///< Usage message for the program
+};  //class CLIBuilder
 
-}
+}   //namespace Stoughton1004Lib
 
 #endif
