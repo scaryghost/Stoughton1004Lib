2bdb7f3bd9498f57ef8bafa473f0aa020f59c7cf
etsai <thegod0fde4th@gmail.com>
2012-09-14 21:38:13 -0700

    Forgot to add central header file

---
+++ CLI/cli.h
@@ -0,0 +1,8 @@
+#ifndef STOUGHTON1004LIB_CLI_H
+#define STOUGHTON1004LIB_CLI_H
+
+#include "Stoughton1004Lib/CLI/Arguments.h"
+#include "Stoughton1004Lib/CLI/CLIBuilder.h"
+#include "Stoughton1004Lib/CLI/Option.h"
+
+#endif
