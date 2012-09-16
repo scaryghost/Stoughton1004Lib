e24e31f9b4038484d6f908fa2cd001f3e5d57169
etsai <thegod0fde4th@gmail.com>
2012-09-16 03:43:24 -0700

    Implemeneted the functions in the template Heap class

--- Heap/Heap.h
+++ Heap/Heap.h
@@ -70,11 +70,132 @@ public:
     unsigned int size() const;
 
 private:
+    /**
+     * Initialize the member variables.  Need this function because C++
+     * does not support constructor delegation yet
+     */
+    void init();
+
     T* elements;                    ///< Pointer to the allocated memory for the heap
     int numElements;                ///< Number of elements stored in the heap
     int capacity;                   ///< Max number of indices available
     Comparator comparator;          ///< Custom comparator used for ordering
+    bool useNaturalOrder;           ///< True if natural ordering should be used
+
+    static const int INIT_CAPACITY; ///< Initial capacity of the heap
 };  //class Heap
+
+template <class T>
+const int Heap<T>::INIT_CAPACITY= 128;
+
+template <class T>
+Heap<T>::Heap() {
+    init();
+    useNaturalOrder= true;
+}
+
+template <class T>
+Heap<T>::Heap(const Comparator& comparator) {
+    init();
+    this->comparator= comparator;
+    useNaturalOrder= false;
+}
+
+template <class T>
+Heap<T>::Heap(const Heap& copy) {
+    init();
+    numElements= copy.numElements;
+    capacity= copy.capacity;
+    comparator= copy.comparator;
+    useNaturalOrder= copy.useNaturalOrder;
+    memcpy(elements, copy.elements, sizeof(T) * copy.capacity);
+}
+
+template <class T>
+Heap<T>::~Heap() {
+    delete[] elements;
+    elements= NULL;
+}
+
+template <class T>
+void Heap<T>::init() {
+    elements= new T[INIT_CAPACITY];
+    numElements= 0;
+    capacity= INIT_CAPACITY;
+}
+
+template <class T>
+void Heap<T>::add(const T& elem) {
+    auto getParent= [](int index) -> { return (index-1)/2; };
+    auto swap= [&this](int index1, int index2) -> void {
+        T temp= elements[index1];
+        elements[index1]= elements[index2];
+        elements[index2]= elements[index1];
+    };
+
+    int parent, index= numElements;
+    elements[numElements]= elem;
+    
+    parent= getParent(index);
+    while(comparator(elements[parent], elements[index]) > 0) {
+        swap(parent, index);
+        index= parent;
+        parent= getParent(index);
+    }
+    numElements++;
+}
+
+template <class T>
+T Heap<T>::remove() {
+    auto getLeftChild= [](int index) -> { return index * 2 + 1; };
+    auto getRightChild= [](int index) -> { return (index + 1) * 2; };
+    auto swap= [&this](int index1, int index2) -> void {
+        T temp= elements[index1];
+        elements[index1]= elements[index2];
+        elements[index2]= elements[index1];
+    };
+    T top= elements[0];
+    int index= 0;
+
+    elements[index]= elements[numElements];
+    numElements--;
+    while(!(elements[index] >= elements[getLeftChild(index)] && elements[index] >= elements[getRightChild(index)])) {
+        if (elements[index] < elements[getLeftChild(index)]) {
+            swap(index, getLeftChild(index));
+            index= getLeftChild(index);
+        } else if (elements[index] > elements[getRightChild(index)]) {
+            swap(index, getRightChild(index));
+            index= getRightChild(index);
+        }
+        if (getLeftChild(index) >= numElements || getRightChild(index) >= numElements)
+            break;
+    }
+    
+}
+
+template <class T>
+bool contains(const T& elem) const {
+    int index;
+    for(index= 0; index < numElements && elements[index] != elem; index++) {
+    }
+
+    return index < numElements;
+}
+
+template <class T>
+bool empty() const {
+    return numElements == 0;
+}
+
+template <class T>
+T top() const {
+    return elements[numElements];
+}
+
+template <class T>
+unsigned int size() const {
+    return numElements;
+}
 
 }   //namespace Stoughton1004Lib
 
