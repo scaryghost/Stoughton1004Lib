7601f8b9e23cd2800ef14cafd298cdb93a4397bd
etsai <thegod0fde4th@gmail.com>
2012-09-20 04:24:26 -0700

    Changed Heap to be an abstract class, implemented by different kinds of heaps

---
+++ Heap/BinaryHeap.h
@@ -0,0 +1,198 @@
+#ifndef STOUGHTON1004LIB_BINARYHEAP_H
+#define STOUGHTON1004LIB_BINARYHEAP_H
+
+#include "Stoughton1004Lib/Heap/Heap.h"
+#include <cstring>
+
+namespace Stoughton1004Lib {
+
+/**
+ * Binary heap implementation of the Heap abstract class
+ * @author etsai
+ */
+template <class T>
+class BinaryHeap : public Heap<T> {
+public:
+    /**
+     * Create a BinaryHeap, ordering the elements according to the custom comparator
+     * @param   comparator  Custom comparator used to ordered the heap
+     */
+    BinaryHeap(const typename Heap<T>::Comparator& comparator);
+    /**
+     * Deallocate the used memory
+     */
+    virtual ~BinaryHeap();
+
+    /**
+     * Add an element to the head.  Operation is O(log n)
+     * @param   elem    Element to add
+     */
+    virtual void add(const T& elem);
+    /**
+     * Remove the top of the heap.  Operation is O(log n)
+     * @return  Top element of the heap
+     */
+    virtual T remove();
+
+    virtual bool contains(const T& elem) const;
+    virtual bool empty() const;
+    /**
+     * Retreive the top element.  Operation is O(1)
+     * @return  Top element
+     */
+    virtual T top() const;
+    virtual unsigned int size() const;
+    /**
+     * Print the contents of the heap.
+     * This does not print the heap in sorted order, just in the order 
+     * the elements reside in the list.
+     * @param   os  Output stream to write to
+     */
+    virtual void print(std::ostream &os) const;
+    /**
+     * Print the contents of the heap with a newline.
+     * This does not print the heap in sorted order, just in the order 
+     * the elements reside in the list.
+     * @param   os  Output stream to write to
+     */
+    virtual void println(std::ostream &os) const;
+
+private:
+    T* elements;                    ///< Pointer to the allocated memory for the heap
+    int numElements;                ///< Number of elements stored in the heap
+    int capacity;                   ///< Max number of indices available
+
+    static const int INIT_CAPACITY; ///< Initial capacity of the heap
+};  //class BinaryHeap
+
+
+template <class T>
+const int BinaryHeap<T>::INIT_CAPACITY= 128;
+
+template <class T>
+BinaryHeap<T>::BinaryHeap(const typename Heap<T>::Comparator& comparator) : Heap<T>(comparator) {
+    elements= new T[INIT_CAPACITY];
+    numElements= 0;
+    capacity= INIT_CAPACITY;
+}
+
+template <class T>
+BinaryHeap<T>::~BinaryHeap() {
+    delete[] elements;
+    elements= nullptr;
+}
+
+template <class T>
+void BinaryHeap<T>::add(const T& elem) {
+    auto getParent= [](int index) -> int { return (index-1)/2; };
+    auto swap= [this](int index1, int index2) -> void {
+        T temp= elements[index1];
+        elements[index1]= elements[index2];
+        elements[index2]= temp;
+    };
+
+    if (numElements == capacity) {
+        T *newList= new T[numElements * 2];
+        std::memcpy(newList, elements, sizeof(T) * capacity);
+        delete[] elements;
+        elements= newList;
+        capacity*= 2;
+    }
+
+    int parent, index= numElements;
+    elements[index]= elem;
+    numElements++;
+
+    parent= getParent(index);
+    while(comparator(elements[parent], elements[index]) < 0) {
+        swap(parent, index);
+        index= parent;
+        parent= getParent(index);
+    }
+}
+
+template <class T>
+T BinaryHeap<T>::remove() {
+    auto getLeftChild= [](int index) -> int { return index * 2 + 1; };
+    auto getRightChild= [](int index) -> int { return (index + 1) * 2; };
+    auto swap= [this](int index1, int index2) -> void {
+        T temp= elements[index1];
+        elements[index1]= elements[index2];
+        elements[index2]= temp;
+    };
+    T top= elements[0];
+    int index= 0;
+
+    elements[index]= elements[numElements-1];
+    numElements--;
+    while(!(comparator(elements[index],elements[getLeftChild(index)]) >= 0 
+            && comparator(elements[index], elements[getRightChild(index)]) >= 0)) {
+
+        if (comparator(elements[index], elements[getLeftChild(index)]) < 0 && 
+            comparator(elements[index], elements[getRightChild(index)]) < 0) {
+            if (comparator(elements[getLeftChild(index)], elements[getRightChild(index)]) > 0 ) {
+                swap(index, getLeftChild(index));
+                index= getLeftChild(index);
+            } else {
+                swap(index, getRightChild(index));
+                index= getRightChild(index);
+            }
+        } else if (comparator(elements[index], elements[getLeftChild(index)]) < 0) {
+            swap(index, getLeftChild(index));
+            index= getLeftChild(index);
+        } else if (comparator(elements[index], elements[getRightChild(index)]) < 0) {
+            swap(index, getRightChild(index));
+            index= getRightChild(index);
+        }
+        if (getLeftChild(index) >= numElements || getRightChild(index) >= numElements)
+            break;
+    }
+    return top;
+}
+
+template <class T>
+bool BinaryHeap<T>::contains(const T& elem) const {
+    int index;
+    for(index= 0; index < numElements && comparator(elements[index],elem) != 0; index++) {
+    }
+
+    return index < numElements;
+}
+
+template <class T>
+bool BinaryHeap<T>::empty() const {
+    return numElements == 0;
+}
+
+template <class T>
+T BinaryHeap<T>::top() const {
+    return elements[0];
+}
+
+template <class T>
+unsigned int BinaryHeap<T>::size() const {
+    return numElements;
+}
+
+template <class T>
+void BinaryHeap<T>::print(std::ostream &os) const {
+    os << "[";
+    for(auto i= 0; i < numElements; i++) {
+        if (i != 0) {
+            os << ", ";
+        }
+        os << elements[i];
+    }
+    os << "]";
+}
+
+template <class T>
+void BinaryHeap<T>::println(std::ostream &os) const {
+    print(os);
+    os << "\n";
+}
+
+
+}   //namespace Stoughton1004Lib
+
+#endif  //STOUGHTON1004LIB_BINARYHEAP_H
--- Heap/Heap.h
+++ Heap/Heap.h
@@ -1,7 +1,6 @@
 #ifndef STOUGHTON1004LIB_HEAP_H
 #define STOUGHTON1004LIB_HEAP_H
 
-#include <cstring>
 #include <functional>
 #include <ostream>
 
@@ -10,13 +9,17 @@ namespace Stoughton1004Lib {
 template <class T>
 
 /**
- * Data structure implementing a heap, or priority queue.  Elements are ordered 
- * based a Comparator.
+ * Abstract class defining common functions shared by all Heap implementations.
+ * Elements are ordered based a Comparator.
  * @author etsai
  */
 class Heap {
 public:
-    /** Type name for the comparator function type */
+    /**
+     * Type name for the comparator function type.  The lambda returns 1 of 3 values: 
+     * a number less than 0 if lhs < rhs, 0 if lhs == rhs, or a number greater than 0 
+     * if lhs > rhs
+     */
     typedef std::function<int (const T&, const T&)> Comparator;
 
     /**
@@ -30,202 +33,70 @@ public:
      */
     Heap(const Heap& copy);
     /**
-     * Deallocate the used memory
+     * Class destructor
      */
-    ~Heap();
+    virtual ~Heap();
 
     /**
      * Add an element to the head
      * @param   elem    Element to add
      */
-    void add(const T& elem);
+    virtual void add(const T& elem)= 0;
     /**
      * Remove the top of the heap
      * @return  Top element of the heap
      */
-    T remove();
+    virtual T remove()= 0;
 
     /**
      * Checks if the element is present in the heap
      * @param   elem    Element to check
      * @return  True if the element is present
      */
-    bool contains(const T& elem) const;
+    virtual bool contains(const T& elem) const= 0;
     /**
      * Checks if the heap is empty
      * @return  True if empty
      */
-    bool empty() const;
+    virtual bool empty() const= 0;
     /**
      * Retreive the top element
      * @return  Top element
      */
-    T top() const;
+    virtual T top() const= 0;
     /**
      * Retreive the number of elements in the heap
      * @return  Heap size
      */
-    unsigned int size() const;
+    virtual unsigned int size() const= 0;
     /**
-     * Print the contents of the heap in list index order.
-     * This does not print the heap in sorted order, just in the order 
-     * the elements reside in the list.
+     * Print the contents of the heap.
      * @param   os  Output stream to write to
      */
-    void print(std::ostream &os) const;
+    virtual void print(std::ostream &os) const= 0;
     /**
-     * Print the contents of the heap in list index order with an newline.
-     * This does not print the heap in sorted order, just in the order 
-     * the elements reside in the list.
+     * Print the contents of the heap with a newline.
      * @param   os  Output stream to write to
      */
-    void println(std::ostream &os) const;
+    virtual void println(std::ostream &os) const= 0;
 
-private:
-    T* elements;                    ///< Pointer to the allocated memory for the heap
-    int numElements;                ///< Number of elements stored in the heap
-    int capacity;                   ///< Max number of indices available
+protected:
     Comparator comparator;          ///< Custom comparator used for ordering
-
-    static const int INIT_CAPACITY; ///< Initial capacity of the heap
 };  //class Heap
 
 template <class T>
-const int Heap<T>::INIT_CAPACITY= 128;
-
-template <class T>
-Heap<T>::Heap(const Comparator& comparator) {
-    elements= new T[INIT_CAPACITY];
-    numElements= 0;
-    capacity= INIT_CAPACITY;
-    this->comparator= comparator;
+Heap<T>::Heap(const Comparator& comparator)
+    :comparator(comparator) {
 }
 
 template <class T>
 Heap<T>::Heap(const Heap& copy) {
-    numElements= copy.numElements;
-    capacity= copy.capacity;
-    comparator= copy.comparator;
-    elements= new T[capacity];
-    std::memcpy(elements, copy.elements, sizeof(T) * copy.capacity);
+    this->comparator= copy.comparator;
 }
 
 template <class T>
 Heap<T>::~Heap() {
-    delete[] elements;
-    elements= nullptr;
 }
-
-template <class T>
-void Heap<T>::add(const T& elem) {
-    auto getParent= [](int index) -> int { return (index-1)/2; };
-    auto swap= [this](int index1, int index2) -> void {
-        T temp= elements[index1];
-        elements[index1]= elements[index2];
-        elements[index2]= temp;
-    };
-
-    if (numElements == capacity) {
-        T *newList= new T[numElements * 2];
-        std::memcpy(newList, elements, sizeof(T) * capacity);
-        delete[] elements;
-        elements= newList;
-        capacity*= 2;
-    }
-
-    int parent, index= numElements;
-    elements[index]= elem;
-    numElements++;
-
-    parent= getParent(index);
-    while(comparator(elements[parent], elements[index]) < 0) {
-        swap(parent, index);
-        index= parent;
-        parent= getParent(index);
-    }
-}
-
-template <class T>
-T Heap<T>::remove() {
-    auto getLeftChild= [](int index) -> int { return index * 2 + 1; };
-    auto getRightChild= [](int index) -> int { return (index + 1) * 2; };
-    auto swap= [this](int index1, int index2) -> void {
-        T temp= elements[index1];
-        elements[index1]= elements[index2];
-        elements[index2]= temp;
-    };
-    T top= elements[0];
-    int index= 0;
-
-    elements[index]= elements[numElements-1];
-    numElements--;
-    while(!(comparator(elements[index],elements[getLeftChild(index)]) >= 0 
-            && comparator(elements[index], elements[getRightChild(index)]) >= 0)) {
-
-        if (comparator(elements[index], elements[getLeftChild(index)]) < 0 && 
-            comparator(elements[index], elements[getRightChild(index)]) < 0) {
-            if (comparator(elements[getLeftChild(index)], elements[getRightChild(index)]) > 0 ) {
-                swap(index, getLeftChild(index));
-                index= getLeftChild(index);
-            } else {
-                swap(index, getRightChild(index));
-                index= getRightChild(index);
-            }
-        } else if (comparator(elements[index], elements[getLeftChild(index)]) < 0) {
-            swap(index, getLeftChild(index));
-            index= getLeftChild(index);
-        } else if (comparator(elements[index], elements[getRightChild(index)]) < 0) {
-            swap(index, getRightChild(index));
-            index= getRightChild(index);
-        }
-        if (getLeftChild(index) >= numElements || getRightChild(index) >= numElements)
-            break;
-    }
-    return top;
-}
-
-template <class T>
-bool Heap<T>::contains(const T& elem) const {
-    int index;
-    for(index= 0; index < numElements && comparator(elements[index],elem) != 0; index++) {
-    }
-
-    return index < numElements;
-}
-
-template <class T>
-bool Heap<T>::empty() const {
-    return numElements == 0;
-}
-
-template <class T>
-T Heap<T>::top() const {
-    return elements[0];
-}
-
-template <class T>
-unsigned int Heap<T>::size() const {
-    return numElements;
-}
-
-template <class T>
-void Heap<T>::print(std::ostream &os) const {
-    os << "[";
-    for(auto i= 0; i < numElements; i++) {
-        if (i != 0) {
-            os << ", ";
-        }
-        os << elements[i];
-    }
-    os << "]";
-}
-
-template <class T>
-void Heap<T>::println(std::ostream &os) const {
-    print(os);
-    os << "\n";
-}
-
 
 }   //namespace Stoughton1004Lib
 
