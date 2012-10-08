abb09d89c85738557ce40fe5fca559053f180799
etsai <thegod0fde4th@gmail.com>
2012-10-07 21:41:01 -0700

    Added central include file for logging module

---
+++ Logging/logging.h
@@ -0,0 +1,10 @@
+#ifndef STOUGHTON1004LIB_LOGGING_H
+#define STOUGHTON1004LIB_LOGGING_H
+
+#include "Stoughton1004Lib/Logging/ConsoleHandler.h"
+#include "Stoughton1004Lib/Logging/Logger.h"
+#include "Stoughton1004Lib/Logging/FileHandler.h"
+#include "Stoughton1004Lib/Logging/Level.h"
+#include "Stoughton1004Lib/Logging/SocketHandler.h"
+
+#endif
